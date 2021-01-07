f1e7933deb41053de244bb04c40c3e288e85c771
mrv96 <mrv96@users.noreply.github.com>
2021-01-07 17:34:13 +0100

    Update pins_LONGER3D_LKx_PRO.h
    Aligned to current https://github.com/MarlinFirmware/Marlin/tree/bugfix-2.0.x

--- Marlin/src/pins/ramps/pins_LONGER3D_LKx_PRO.h
+++ Marlin/src/pins/ramps/pins_LONGER3D_LKx_PRO.h
@@ -25,25 +25,21 @@
  * Longer3D LK1/LK4/LK5 Pro board pin assignments
  */
 
-#ifndef BOARD_LONGER3D_LK1_PRO
-  #error "BOARD_LONGER3D_LK1_PRO no longer defined. Remember to change all MB(LONGER3D_LK1_PRO) occurences."
-#endif
-
 #if NOT_TARGET(__AVR_ATmega2560__)
   #error "Oops! Select 'Arduino/Genuino Mega or Mega 2560' in 'Tools > Board.'"
 #elif HOTENDS > 1 || E_STEPPERS > 1
-  #error "Longer3D LGT KIT V1.0 board only supports 1 hotend / E-stepper. Comment out this line to continue."
+  #error "Longer3D LGT KIT V1.0 board only supports one hotend / E-stepper. Comment out this line to continue."
 #endif
 
 #if SERIAL_PORT == 1 || SERIAL_PORT_2 == 1
   #warning "Serial 1 is originally reserved to DGUS LCD."
 #endif
 #if SERIAL_PORT == 2 || SERIAL_PORT_2 == 2
-  #warning "Serial 2 has not a connector. To get it working, hardware changes are required."
+  #warning "Serial 2 has no connector. Hardware changes may be required to use it."
 #endif
 #if SERIAL_PORT == 3 || SERIAL_PORT_2 == 3
   #define CHANGE_Y_LIMIT_PINS
-  #warning "Serial 3 is originally reserved to Y limit switches. To get it working, hardware changes are required."
+  #warning "Serial 3 is originally reserved to Y limit switches. Hardware changes are required to use it."
 #endif
 
 // Custom flags and defines for the build
@@ -52,66 +48,32 @@
 #define BOARD_INFO_NAME "LGT KIT V1.0"
 
 //
-// Misc. Functions
-//
-#define SD_DETECT_PIN                         49
-#define FIL_RUNOUT_PIN                         2
-
-//
-// Other RAMPS 1.3 pins
-//
-#define IS_RAMPS_EFB                              // Override autodetection. Bed will be undefined.
-#include "pins_RAMPS_13.h"
-
-//
 // Servos
 //
-#ifdef SERVO0_PIN
-  #undef SERVO0_PIN
-#endif
-#ifdef SERVO1_PIN
-  #undef SERVO1_PIN
-#endif
-#ifdef SERVO2_PIN
-  #undef SERVO2_PIN
-#endif
-#ifdef SERVO3_PIN
-  #undef SERVO3_PIN
-#endif
-
 #if !MB(LONGER3D_LK1_PRO)
   #define SERVO0_PIN                           7
 #endif
+#define SERVO1_PIN -1
+#define SERVO2_PIN -1
+#define SERVO3_PIN -1
 
 //
 // Limit Switches
 //
-#ifdef X_MAX_PIN
-  #undef X_MAX_PIN
-#endif
-#ifdef Y_MIN_PIN
-  #undef Y_MIN_PIN
-#endif
-#ifdef Y_MAX_PIN
-  #undef Y_MAX_PIN
-#endif
-#ifdef Z_MIN_PIN
-  #undef Z_MIN_PIN
-#endif
-#ifdef Z_MAX_PIN
-  #undef Z_MAX_PIN
-#endif
+#define X_STOP_PIN                             3
 
-#ifndef CHANGE_Y_LIMIT_PINS
+#ifdef CHANGE_Y_LIMIT_PINS
+  #define Y_STOP_PIN                          37
+#else
   #define Y_MIN_PIN                           14
   #define Y_MAX_PIN                           15
-#else
-  #define Y_MIN_PIN                           37
 #endif
 
 #if !MB(LONGER3D_LK1_PRO)
-  #define Z_MIN_PIN                           35
-  #ifndef CHANGE_Y_LIMIT_PINS
+  #ifdef CHANGE_Y_LIMIT_PINS
+    #define Z_STOP_PIN                        35
+  #else
+    #define Z_MIN_PIN                         35
     #define Z_MAX_PIN                         37
   #endif
 #else
@@ -122,40 +84,47 @@
 //
 // Z Probe (when not Z_MIN_PIN)
 //
-#ifdef Z_MIN_PROBE_PIN
-  #undef Z_MIN_PROBE_PIN
+#define Z_MIN_PROBE_PIN                       -1
+
+//
+// SD Support
+//
+#if SD_CONNECTION_IS(LCD)
+  #error "SD LCD is not compatible with LONGER3D_LKx_PRO"
+#elif SD_CONNECTION_IS(ONBOARD)
+  #define SD_DETECT_PIN                       -1
+#elif SD_CONNECTION_IS(LCD)
+  #error "SD CUSTOM_CABLE is not compatible with LONGER3D_LKx_PRO"
+#else
+  #define SD_DETECT_PIN                       49
 #endif
+
+//
+// Misc. Functions
+//
+#define FIL_RUNOUT_PIN                         2
+
+//
+// Other RAMPS 1.3 pins
+//
+#define IS_RAMPS_EFB                              // Override autodetection. Bed will be undefined.
+#include "pins_RAMPS_13.h"
 
 //
 // Steppers
 //
-#ifdef E1_STEP_PIN
-  #undef E1_STEP_PIN
-#endif
-#ifdef E1_DIR_PIN
-  #undef E1_DIR_PIN
-#endif
-#ifdef E1_ENABLE_PIN
-  #undef E1_ENABLE_PIN
-#endif
-#ifdef E1_CS_PIN
-  #undef E1_CS_PIN
-#endif
+#undef E1_STEP_PIN
+#undef E1_DIR_PIN
+#undef E1_ENABLE_PIN
+#undef E1_CS_PIN
 
 //
 // Temperature Sensors
 //
-#ifdef TEMP_1_PIN
-  #undef TEMP_1_PIN
-#endif
+#undef TEMP_1_PIN
 
 //
 // Průša i3 MK2 Multiplexer Support
 //
-#ifdef E_MUX2_PIN
-  #undef E_MUX2_PIN
-#endif
-
-#ifdef CHANGE_Y_LIMIT_PINS
-  #undef CHANGE_Y_LIMIT_PINS
-#endif
+#undef E_MUX2_PIN
+#undef CHANGE_Y_LIMIT_PINS
